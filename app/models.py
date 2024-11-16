114bba5bd7e43f8683cecb85992270e424cc496a
Sujan <117112001+Sujan-coder-sudo@users.noreply.github.com>
2024-11-16 09:53:59 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class React(models.Model):
-    employee = models.CharField(max_length=50)
-    department = models.CharField(max_length=60)
-    
